b84d4e187492b40969ff0f608f8810f3ec3b7f48
Albpenu <albpenu3110@gmail.com>
2018-01-11 10:20:16 +0100

    Actualizacion

---
+++ inserts.sql
@@ -0,0 +1,23 @@
+insert into categorias values (NULL, 'Aspirante a investigador', 'Para usuarios iniciados en esto de las movidas científicas');
+insert into categorias values (NULL, 'Investigador auxiliar', 'Para usuarios que pilotan de ciencia');
+insert into categorias values (NULL, 'Investigador titular', 'Sólo apta para usuarios duchos, sabios o eminentes; incluso pedantes ;)');
+
+/*insert into usuarios values (NULL, 'Hawking1996', md5('1234'), '');
+insert into usuarios values (NULL, 'TeslaCharger98', 'Usuario  ciencia');
+insert into usuarios values (NULL, 'MarieCurieTodoNobels', '');
+insert into usuarios values (NULL, 'NewtonOrOldton', '');*/
+/* docker runn -it -d -p 8080 -p 3316:3306 */
+insert into posts values (NULL, NULL, NULL, '', '', '', '', '');
+insert into posts values (NULL, NULL, NULL, '', '', '', '', '');
+insert into posts values (NULL, NULL, NULL, '', '', '', '', '');
+insert into posts values (NULL, NULL, NULL, '', '', '', '', '');
+
+insert into subcategorias values (NULL, NULL,'', '');
+insert into subcategorias values (NULL, NULL,'', '');
+insert into subcategorias values (NULL, NULL,'', '');
+insert into subcategorias values (NULL, NULL,'', '');
+
+insert into votos values (NULL, '', NULL, NULL);
+insert into votos values (NULL, '', NULL, NULL);
+insert into votos values (NULL, '', NULL, NULL);
+insert into votos values (NULL, '', NULL, NULL);
